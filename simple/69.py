07c9cb0ff8ffa944d5a9142463c9b4c3cf114510
issac.chen <issac.chen@vipshop.com>
2020-09-16 16:33:35 +0800

    some

---
+++ simple/69.py
@@ -0,0 +1,38 @@
+"""
+实现int sqrt(int x)函数。
+计算并返回x的平方根，其中x 是非负整数。
+由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+
+示例 1:
+输入: 4
+输出: 2
+
+示例 2:
+输入: 8
+输出: 2
+说明: 8 的平方根是 2.82842..., 
+由于返回类型是整数，小数部分将被舍去。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/sqrtx
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left, right, ans = 0, x, -1
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid <= x:
+                ans = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        return ans
+
+
+if __name__ == '__main__':
+    assert Solution().mySqrt(1) == 1
+    assert Solution().mySqrt(4) == 2
+    assert Solution().mySqrt(8) == 2
